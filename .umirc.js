80a3ed18539a5c1fc7ce330e7378cc9894ed6126
fangmw <fangmw@lichengsoft.com>
2019-10-25 15:16:29 +0800

    init

---
+++ .umirc.js
@@ -0,0 +1,59 @@
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  routes: [
+    {
+      path: '/',
+      component: '../layouts/index',
+      routes: [
+        {
+          name: '查询表格',
+          path: '/listtablelist',
+          component: './ListTableList',
+        },
+        {
+          name: '登录页',
+          path: '/userlogin',
+          component: './UserLogin',
+        },
+        {
+          name: '个人设置',
+          path: '/accountsettings',
+          component: './AccountSettings',
+        },
+        {
+          path: '/',
+          component: '../pages/index',
+        },
+      ],
+    },
+  ],
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    [
+      'umi-plugin-react',
+      {
+        antd: true,
+        dva: true,
+        dynamicImport: {
+          webpackChunkName: true,
+        },
+        title: 'licheng-cloud-client',
+        dll: false,
+        routes: {
+          exclude: [
+            /models\//,
+            /services\//,
+            /model\.(t|j)sx?$/,
+            /service\.(t|j)sx?$/,
+            /components\//,
+          ],
+        },
+        locale: {
+          default: 'zh-CN',
+        },
+      },
+    ],
+  ],
+  publicPath: './',
+};
